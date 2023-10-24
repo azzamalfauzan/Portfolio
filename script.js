7a478df4c25c2a60dc9aab5336dca32aa7c340e2
unknown <yusufazzamidnsolo@gmail.com>
2023-10-24 08:22:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// toggle & responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navlist = document.querySelector("nav");
+  
+    burger.addEventListener("click", () =>{
+  
+      navlist.classList.toggle("nav-active");
+      burger.classList.toggle("toggle-burger");
+  });
+  }
+  
+  navSlide();
+  
+  // hapus form setelah diisi
+  window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+      form.reset();
+    }
+  }
+
+// hapus form setelah diisi
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+      form.reset();
+    }
+  }
+  
